2593b74f1ef5e233c312da10902ca1f801402c66
Туркова Елизавета Николаевна <liza_lisa_05@mail.ru>
2024-11-11 00:22:57 +0700

    SFML

---
+++ Game.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <SFML/Graphics.hpp>
+
+
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(900, 900), "MyGame");
+
+
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		window.clear(Color::White);
+
+		window.display();
+	}
+
+
+	return 0;
+
+
+
+
+}
