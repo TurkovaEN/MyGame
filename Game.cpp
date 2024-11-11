27120996deeca2f4492deb271f89ff94efa725a5
Туркова Елизавета Николаевна <liza_lisa_05@mail.ru>
2024-11-12 00:59:37 +0700

    Add sprite

--- Game.cpp
+++ Game.cpp
@@ -6,25 +6,51 @@ using namespace sf;
 
 int main()
 {
-	RenderWindow window(VideoMode(900, 900), "MyGame");
+	RenderWindow window(VideoMode(900, 900), "MyGame"); //создание окна
 
+	Sprite s;
+	Texture t;
+	t.loadFromFile("sprite1.png"); //добавление текстуры спрайта из файла
+	s.setTexture(t);
+	s.setTextureRect(IntRect(253, 286, 120, 150));
 
+	s.setPosition(50, 100); //установка спрайта на карту
 
 	while (window.isOpen())
 	{
-		Event event;
+		Event event; //обработка событий
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed)
+			if (event.type == Event::Closed) //закрытие окна, нажатием на крестик
 				window.close();
 		}
-		window.clear(Color::White);
 
-		window.display();
+		//движение на стрелочки
+
+		if (Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			s.move(-0.1, 0);
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			s.move(0.1, 0);
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+		{
+			s.move(0, 0.1);
+		}
+
+		window.clear(Color::White); //задний фон окна
+		window.draw(s); //вывод спрайта
+		window.display(); //обновление содержимого окна
 	}
 
 
 	return 0;
+
+
 
 
 }
