7531b5197ab8dc351daa07ee9d42f139557468f5
Туркова Елизавета Николаевна <liza_lisa_05@mail.ru>
2024-11-18 22:08:31 +0700

    Карта рабочая, добавлена обработка столкновений, скорректирован размер окна под карту

--- Game.cpp
+++ Game.cpp
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <SFML/Graphics.hpp>
 
-int ground = 550;
+int ground = 580;
 using namespace sf;
 
-const int H = 12;
+const int H = 23;
 const int W = 50;
 
 String TileMap[H] = {
 	"#################################################",
 	"#                                               #",
+	"#                                               #",
+	"#                                               #",
+	"#                                               #",
 	"#  K                                         E  #",
 	"#########                              ##########",
+	"#                                               #",
+	"#                                               #",
+	"#                                               #",
 	"#                                               #",
 	"#                                               #",
 	"#           #######         #########           #",
 	"#                                               #",
 	"#                                               #",
+	"#                                               #",
+	"#                                               #",
 	"#                    ####                       #",
 	"#                                               #",
-	"#################################################",
+	"#                                               #",
+	"#                                               #",
+	"#                                               #",
+	"#################################################"
 };
 
 class PLAYER {
@@ -34,6 +45,8 @@ public:
 	{
 		sprite.setTexture(image);
 		rect = FloatRect(220, 189, 158, 112); //rect (x,y, width, height), (x,y) - координаты левого верхнего угла, width - ширина, height - высота
+		
+
 
 		dx = dy = 0.1;
 		currentFrame = 0;
@@ -43,22 +56,14 @@ public:
 	{
 		
 		rect.left += dx * time; //координата х
-	
+		Collision(0);
 
 		if (!onGround) 
 			dy = dy + 0.0005 * time; //ускорение свободного падения при прыжке
 		rect.top += dy * time; //координата у
 		onGround = false;
+		Collision(1);
 
-		//прыжок
-		if (rect.top > ground)
-		{
-			rect.top = ground; 
-			dy = 0;
-			onGround = true;
-		}
-		
-		
 
 		currentFrame += 0.005 * time;
 		if (currentFrame > 7)
@@ -74,11 +79,35 @@ public:
 		dx = 0;
 	}
 
+	//обработка столкновений
+	void Collision(int dir)
+	{
+		for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
+			for (int j = rect.left / 32; j<(rect.left+rect.width)/32; j++) {
+				if (TileMap[i][j] == '#') {
+
+					if ((dx > 0)&&(dir==0)) rect.left = j * 32 - rect.width;
+					if ((dx < 0) && (dir == 0)) rect.left = j * 32 + 32;
+
+					if ((dy > 0) && (dir == 1)) {
+						rect.top = i * 32 - rect.height;
+						dy = 0;
+						onGround = true;
+					}
+					if ((dy < 0) && (dir == 1)) {
+						rect.top = i * 32 + 32;
+						dy = 0;
+					}
+				}
+
+			}
+	}
+	
 };
 
 int main()
 {
-	RenderWindow window(VideoMode(2500, 1250), "MyGame"); //создание окна
+	RenderWindow window(VideoMode(1600, 735), "MyGame"); //создание окна
 	Texture t;
 	t.loadFromFile("sprite_character.png"); //добавление текстуры спрайта из файла
 
@@ -89,6 +118,7 @@ int main()
 	Clock clock;
 
 	RectangleShape rectangle(Vector2f(32,32));
+
 
 	while (window.isOpen())
 	{
@@ -119,7 +149,7 @@ int main()
 		if (Keyboard::isKeyPressed(Keyboard::Up))
 		{
 			if (p.onGround) {
-				p.dy = -0.4; //высота прыжка
+				p.dy = -0.45; //высота прыжка
 				p.onGround = false;
 			}
 		}
